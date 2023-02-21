0479a6aa77e22f9b53bdc9eae97125f1756fab79
nanjingrjb@gmail.com <nanjingrjb@gmail.com>
2023-02-21 08:51:41 +0800

    对于元组tuple也可执行in遍历操作

--- Chapter02/listing2-4.py
+++ Chapter02/listing2-4.py
@@ -1,11 +1,11 @@
 # Check a user name and PIN code
 
-database = [
+database = (
     ['albert',  '1234'],
     ['dilbert', '4242'],
     ['smith',   '7524'],
     ['jones',   '9843']
-]
+    )
 
 username = input('User name: ')
 pin = input('PIN code: ')
