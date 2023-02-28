a16321f90a9a93e59ded7b69316eb1f8b0c78eee
nanjingrjb <nanjingrjb@gmail.com>
2023-02-28 18:48:01 +0800

    将e转化为str，再输出。

--- Chapter19/listing19_3.py
+++ Chapter19/listing19_3.py
@@ -8,9 +8,9 @@ logging.info('Trying to divide 1 by 0')
 try:
     print(1 / 0)
 except Exception as e:
-    logging.info('发生异常，信息为:')
+    logging.info('发生异常，信息为:'+str(e))
     
-    logging.info(e)
+    #logging.info(e)
 
 logging.info('The division succeeded')
 
