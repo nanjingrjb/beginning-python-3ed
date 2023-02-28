15532aa03ff8e96027b5f4afe77f80463618d238
nanjingrjb <nanjingrjb@gmail.com>
2023-02-28 18:12:12 +0800

    调用logging模块，记录信息，并处理异常。

--- Chapter19/listing19_3.py
+++ Chapter19/listing19_3.py
@@ -5,8 +5,10 @@ logging.basicConfig(level=logging.INFO, filename='mylog.log')
 logging.info('Starting program')
 
 logging.info('Trying to divide 1 by 0')
-
-print(1 / 0)
+try:
+    print(1 / 0)
+except Exception as e:
+    logging.info('发生异常，信息为:',e)
 
 logging.info('The division succeeded')
 
